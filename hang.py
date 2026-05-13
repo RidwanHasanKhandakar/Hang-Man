1461095e82aa50625a278afd77683bcea2fa3b9b
Ridwan Hasan Khandakar <2310604@iub.edu.bd>
2026-05-13 23:33:13 +0600

    did the ASCII art.

--- hang.py
+++ hang.py
@@ -1,2 +1,24 @@
 import random
 words=("apple" "banana" "cherry" "date" "elderberry" "fig" "grape" "honeydew")
+hangman_art={0:("   ",
+                "   ",
+                "   "), 
+             1:(" o ",
+                "   ",
+                "   "), 
+             2:(" o ",
+                " | ",
+                "   "), 
+             3:(" o ",
+                "/| ",
+                "   "), 
+             4:(" o ",
+                "/|\\",
+                "   "), 
+             5:(" o ",
+                "/|\\ ",
+                "/  "), 
+             6:(" o ",
+                "/|\\ ",
+                "/ \\")}
+print(hangman_art[6])
